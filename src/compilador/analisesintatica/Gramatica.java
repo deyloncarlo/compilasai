ec9f94143cb578516ff36b70f8d157b21171a43c
Deylon Carlo <deyloncarlo@gmail.com>
2017-10-13 23:55:41 -0300

    implementando a gramática - declaração de variáveis

--- src/compilador/analisesintatica/Gramatica.java
+++ src/compilador/analisesintatica/Gramatica.java
@@ -19,27 +19,40 @@ public class Gramatica
 		AnalisadorSintatico.casaToken(p_token);
 	}
 
-	public void DECLARACAO_VARIAVEIS() throws IOException
-	{
-		TIPO();
-		casaToken(Token.IDENTIFICADOR);
-		FORMA_DECLARACAO();
-		casaToken(Token.PONTO_VIRGULA);
-	}
-
 	/**
-	 * Identifica como será feita a declaracao de veriável
+	 * Classe responsável por gerenciar parte de declaração de variáveis
 	 * 
-	 * @throws IOException
+	 * @author Deylon
+	 *
 	 */
-	private void FORMA_DECLARACAO() throws IOException
+	private class DeclaracaoVariaveis
 	{
-		if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.VIRGULA))
+		/**
+		 * Parte responsável por identificar todas as declaraçoes de variaveis
+		 * 
+		 * @throws IOException
+		 */
+		public void PARTE_DECLARACAO_VARIAVEIS() throws IOException
 		{
-			LISTA_ID_DECLARACAO();
+			if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.FINAL))
+			{
+				DECLARACAO_FINAL();
+			}
+			else
+			{
+				DECLARACAO_VARIAVEIS();
+			}
 		}
-		else
+
+		/**
+		 * Identifica a declaracao de constantes (FINAL)
+		 */
+		private void DECLARACAO_FINAL() throws IOException
 		{
+			casaToken(Token.FINAL);
+			casaToken(Token.IDENTIFICADOR);
+			casaToken(Token.IGUAL);
+
 			if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.ADICAO))
 			{
 				casaToken(Token.ADICAO);
@@ -48,43 +61,86 @@ public class Gramatica
 			{
 				casaToken(Token.SUBTRACAO);
 			}
-
 			casaToken(Token.CONSTANTE);
 		}
-	}
 
-	/**
-	 * Identifica declaração de lista de variáveis
-	 * 
-	 * @throws IOException
-	 */
-	private void LISTA_ID_DECLARACAO() throws IOException
-	{
-		casaToken(Token.VIRGULA);
-		casaToken(Token.IDENTIFICADOR);
-		if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.VIRGULA))
+		/**
+		 * Reconhece as declaraçoes de variaveis
+		 * 
+		 * @throws IOException
+		 */
+		public void DECLARACAO_VARIAVEIS() throws IOException
 		{
-			LISTA_ID_DECLARACAO();
+			TIPO();
+			casaToken(Token.IDENTIFICADOR);
+			FORMA_DECLARACAO();
+			casaToken(Token.PONTO_VIRGULA);
 		}
-	}
 
-	private void TIPO() throws IOException
-	{
-		if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.INT))
+		/**
+		 * Identifica como será feita a declaracao de veriável
+		 * 
+		 * @throws IOException
+		 */
+		private void FORMA_DECLARACAO() throws IOException
 		{
-			casaToken(Token.INT);
+			if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.VIRGULA))
+			{
+				LISTA_ID_DECLARACAO();
+			}
+			else
+			{
+				if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.ADICAO))
+				{
+					casaToken(Token.ADICAO);
+				}
+				else if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.SUBTRACAO))
+				{
+					casaToken(Token.SUBTRACAO);
+				}
+
+				casaToken(Token.CONSTANTE);
+			}
 		}
-		else if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.STRING))
+
+		/**
+		 * Identifica declaração de lista de variáveis
+		 * 
+		 * @throws IOException
+		 */
+		private void LISTA_ID_DECLARACAO() throws IOException
 		{
-			casaToken(Token.STRING);
+			casaToken(Token.VIRGULA);
+			casaToken(Token.IDENTIFICADOR);
+			if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.VIRGULA))
+			{
+				LISTA_ID_DECLARACAO();
+			}
 		}
-		else if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.BOOLEAN))
+
+		/**
+		 * Identifica qual o tipo da variavel durante a declaracao
+		 * 
+		 * @throws IOException
+		 */
+		private void TIPO() throws IOException
 		{
-			casaToken(Token.BOOLEAN);
-		}
-		else if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.BYTE))
-		{
-			casaToken(Token.BYTE);
+			if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.INT))
+			{
+				casaToken(Token.INT);
+			}
+			else if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.STRING))
+			{
+				casaToken(Token.STRING);
+			}
+			else if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.BOOLEAN))
+			{
+				casaToken(Token.BOOLEAN);
+			}
+			else if (AnalisadorLexico.getRegistroLexico().getToken().equals(Token.BYTE))
+			{
+				casaToken(Token.BYTE);
+			}
 		}
 	}
 
